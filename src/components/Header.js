5986f644b761074b80145d9111259a8623e26cce
Akshay.B <akshay.b@mountblue.tech>
2020-10-10 13:02:46 +0530

    Added Header component.

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import '../styles/Header.css'
+
+function Header(){
+    // const [task,setTask] = useState([]);
+    // setTask(1)
+    return(
+        <div>
+        <header className='header'>TODO List</header>
+        <div>
+            {/* {task} */}
+        </div>
+        </div>
+    )
+}
+
+export default Header;
