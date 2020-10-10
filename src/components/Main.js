5634727ea63778d99d2f8563fa6ad94540658661
Akshay.B <akshay.b@mountblue.tech>
2020-10-10 16:04:17 +0530

    Added ModalView and ModalInnerView components to add, update, and delete todosList.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import TypeSelector from './TypeSelector'
-import TodoList from './TodoList'
+import TypeSelector from './TypeSelector';
+import TodoList from './TodoList';
+import ModalView from './ModalView';
+import ModalInnerView from './ModalInnverView'
 
 class Main extends Component {
     constructor(props) {
@@ -10,7 +12,7 @@ class Main extends Component {
           description: "",
           selectedTodoType: "all",
           todoTypes: ["all", "pending", "completed"],
-          isAddCardOpen: false,
+          isModalOpen: false,
           allTodos: JSON.parse(localStorage.getItem("todos")) || [],
 
 
@@ -33,10 +35,76 @@ class Main extends Component {
         }
       };
     
+      onTitleChange = title => {
+        this.setState({ title });
+      };
+
+      onDescChange = description => {
+        this.setState({ description });
+      };
+
+      addTodo = () => {
+        let todosList = [];
+        let todoItem = {};
+        const { title, description } = this.state;
+        if (!title) return;
+        const previousList = JSON.parse(localStorage.getItem("todos"));
+        if (previousList !== "" && Array.isArray(previousList)) {
+          todosList = [...previousList];
+        }
+        const d = new Date();
+        const id = d.valueOf();
+        todoItem = { id: id, title, description, completed: false };
+        todosList.push(todoItem);
+        this.updateLocalStorage(todosList);
+        this.updateTodo(todosList);
+        this.toggleModal();
+        this.resetData();
+      };
+
+
+  completedToDo = id => {
+    const { allTodos } = this.state;
+    const todoIndex = this.state.allTodos.findIndex(todo => todo.id == id);
+    allTodos[todoIndex].completed = true;
+    this.updateTodo(allTodos);
+    this.updateLocalStorage(allTodos);
+  };
+
+      updateTodo = allTodos => {
+        this.setState({ allTodos });
+      };
+    
+      updateLocalStorage = todoList => {
+        localStorage.setItem("todos", JSON.stringify(todoList));
+      };
+
+      toggleModal = () => {
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+      };
+    
+      resetData = () => {
+        this.setState({ title: "", description: "" });
+      };
+    
+      updateTodo = allTodos => {
+        this.setState({ allTodos });
+      };
+    
+      updateLocalStorage = todoList => {
+        localStorage.setItem("todos", JSON.stringify(todoList));
+      };
+    
+      deleteTodo = id => {
+        const newList = this.state.allTodos.filter(todos => todos.id !== id);
+        this.updateLocalStorage(newList);
+        this.updateTodo(newList);
+      };
+    
       render(){
           const {
           title,
-          isAddCardOpen,
+          isModalOpen,
           description,
           selectedTodoType,
           todoTypes,
@@ -49,8 +117,18 @@ class Main extends Component {
           return (
               <div className='todoContainer'>
                 <TypeSelector buttonArray={todoTypes} onClick={this.showTodosOfSelectedType} btnActive={selectedTodoType} />
-                <TodoList todos={listOfTodos} deleteTodo={this.deleteTodo} completedToDo={this.completedToDo}
-          />
+                <TodoList todos={listOfTodos} deleteTodo={this.deleteTodo} completedToDo={this.completedToDo} />
+                <ModalView isVisible={isModalOpen}>
+                <ModalInnerView
+                    title={title}
+                    description={description}
+                    onTitleChange={this.onTitleChange}
+                    OnDescChange={this.onDescChange}
+                    add={this.addTodo}
+                    reset={this.resetData}
+                    cancel={this.toggleModal}
+                />
+                </ModalView>
               </div>
           );
       }
