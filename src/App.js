443b96efbeed27a4a6d2b8525ce642cf830d7f0d
Akshay.B <akshay.b@mountblue.tech>
2020-10-13 00:18:26 +0530

    Added Homepage component and routes in App.js for Homepage and Main components.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header.js"
 import Main from "./components/Main"
+import Homepage from "./components/Homepage"
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Route exact path='/' component={Homepage} />
+      <Route path='/main' component={Main} />
+      {/* <Homepage /> */}
      {/* <Header /> */}
-     <Main />
+     {/* <Main /> */}
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+ 
+ function Homepage() {
+     return (
+        <div>
+            <h1>WELCOME !</h1>
+            <Button variant="contained" color="secondary">
+                LOGIN
+            </Button>
+            <Button variant="contained" color="secondary">
+                SIGN-UP
+            </Button>
+        </div>
+     )
+ }
+ 
+ export default Homepage
+ 
