03fe425a93bd790eac52a9ff1135c29c8e6ac800
Akshay.B <akshay.b@mountblue.tech>
2020-10-20 01:41:38 +0530

    Added Task and User models.

---
+++ server/models/Task.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../database/db.js')
+
+module.exports = db.sequelize.define(
+  'task',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: Sequelize.STRING
+    },
+    description:{
+      type: Sequelize.STRING  
+    },
+    status: {
+      type: Sequelize.STRING
+    },
+    user_id: {
+      type: Sequelize.INTEGER
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  },
+  {
+    timestamps: false
+  }
+)
