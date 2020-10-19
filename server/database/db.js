c1b2e6735179274d6b9fe1f8ff887b26dd7ba9e2
Akshay.B <akshay.b@mountblue.tech>
2020-10-20 01:07:18 +0530

    Config database and initialise database.

---
+++ server/database/db.js
@@ -0,0 +1,21 @@
+const config = require('./db.config.js');
+const Sequelize = require('sequelize')
+const db = {}
+
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
